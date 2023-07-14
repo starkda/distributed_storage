2698254e004a3552ed282a2c0104a41d9a134795
starkda <d.vakhidov@innopolis.university>
2023-07-14 17:50:05 +0300

    lul

--- sender.py
+++ sender.py
@@ -4,19 +4,19 @@ import time
 import grpc
 import data_transfer_api_pb2 as service
 import data_transfer_api_pb2_grpc as stub
-def put(key, value):
+def put(key, value, stub):
     val = service.Value(payload=bytes(str(value), encoding="UTF-8"))
     args = service.StoreValueRequest(key=key, value=val)
     stub.StoreValue(args)
 
 
-def get():
+def get(stub):
     args = service.GetValueRequest(key="hello")
     response = stub.GetValue(args)
     print(f"{response.value}")
 
 
-def delete_user(user_id):
+def delete_user(user_id, stub):
     args = service.User(user_id=user_id)
     response = stub.DeleteUser(args)
     print(f"DeleteUser({user_id}) = {response.status}")
@@ -37,8 +37,8 @@ if __name__ == '__main__':
     parser.add_argument("freq")
     args = parser.parse_args()
 
-    with grpc.insecure_channel('localhost:1234') as channel:
+    with grpc.insecure_channel('0.0.0.0:1234') as channel:
         stub = stub.KeyValueServiceStub(channel)
         key = args.key
         freq = float(args.freq)
-        send_sin_wave(key, freq)
+        send_sin_wave(key, freq, stub)
